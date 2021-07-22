ad5e2b824b70c17b3ac2cf6f2561b88ebf23a102
HyeonseungSon <boywin8636@gmail.com>
2021-07-22 16:46:47 +0900

    2021.07.22 16:46

--- src/Main.kt
+++ src/Main.kt
@@ -4,4 +4,14 @@
 
 fun main() {
 
+    val v1 = 100
+    val r1 = 100 add 50
+    println("r1 : $r1")
+
+    val r2 = v1 add 50
+    println("r2 : $r2")
+}
+
+infix fun Int.add(a1:Int) : Int {
+    return this + a1
 }
